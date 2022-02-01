cec2b5b663add57dc054a4fdb34b5c5fd9be5a7e
Kayce Heap <kayce.heap@gmail.com>
2022-01-31 20:14:20 -0700

    got managers working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
 
 const getUserRequest = function() {
     inquirer.prompt( {
@@ -6,7 +8,63 @@ const getUserRequest = function() {
         name: "request",
         message: "What would you like to do?"
     }).then(({ request }) => {
+        if (request.toLowerCase() == "view departments") {
+            showDepartments();
+        } 
+        if (request.toLowerCase() == "view roles") {
+            showRoles();
+        }
+        if (request.toLowerCase() == "view employees") {
+            showEmployees();
+        }
         console.log(request);
+      
+    })
+}
+const getConnection = function() {
+    const connection = mysql.createConnection( {
+        host: "localhost", 
+        user: "root",
+        password: "coconuts",
+        database: "Coconut"
+    });
+    return connection;
+   
+    
+}
+
+const showDepartments = function() {
+    const connection = getConnection();
+    connection.query("select * from departments", function(err, results, fields) {
+        console.table(results);
+        getUserRequest();
+    })
+};
+
+const showRoles = function() {
+    const connection = getConnection();
+    connection.query("select * from roles", function(err, results, fields) {
+        console.table(results);
+        getUserRequest();
+        
+    })
+    
+}
+
+const showEmployees = function() {
+    const connection = getConnection();
+    connection.query(`select emp.id, emp.first_name, emp.last_name, r.title, d.name, r.salary, manager.first_name as 'manager first name',
+     manager.last_name as 'manager last name' from employees emp
+     join roles r on r.id = emp.role_id 
+     join departments d on d.id = r.department_id 
+     left outer join employees manager on manager.id = emp.manager_id 
+    `, function(err, results, fields) {
+        if (err) {
+            console.log(err);
+        }
+        console.table(results);
+        getUserRequest();
+        
     })
 }
 
